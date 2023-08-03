0386157fa77192c6857e362eaa1a0ac1dd6444f1
Hava Elboim <77002425+HavaElboim@users.noreply.github.com>
2023-08-03 08:32:54 +0300

    Added styling to cart

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -7,6 +7,9 @@ import { BooksService } from '../books.service';
   styleUrls: ['./cart.component.scss']
 })
 export class CartComponent {
+  // @HostBinding lets you set properties on the element or component that hosts the directive
+  // see answer 5 in: https://stackoverflow.com/questions/34645315/how-to-apply-css-class-to-a-component-element-when-its-created-by-router-outlet
+  // and https://www.digitalocean.com/community/tutorials/angular-hostbinding-hostlistener
   booksService = inject(BooksService);
   cart = this.booksService.cart;
 }
